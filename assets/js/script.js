d3c21af3195e9dcadb1b473c8e67bf7d10560942
Asraful Hoque <67984098+AsrafulHoque@users.noreply.github.com>
2021-11-18 10:46:53 +0600

    Add files via upload
    Sign in page

---
+++ assets/js/script.js
@@ -0,0 +1,51 @@
+/**
+ * @Script js for (Template/Project Name)
+ *
+ * @project     - Project Name
+ * @author      - 
+ * @created_by  - 
+ * @created_at  - 
+ * @modified_by -
+ */
+
+
+/**
+ * ========================================================
+ * this function execute when window properly loaded
+ * ===========================================================
+ */
+
+$(window).on('load', function () {
+
+    // code should be execute here
+
+});
+
+
+
+/**
+ * ========================================================
+ * this function execute when DOM element ready 
+ * ===========================================================
+ */
+
+$(document).ready(function () {
+
+    // code should be execute here
+
+    $(function () {
+        $("#show_hide_password a").on('click', function (event) {
+            event.preventDefault();
+            if ($('#show_hide_password input').attr("type") == "text") {
+                $('#show_hide_password input').attr('type', 'password');
+                $('#show_hide_password i').addClass("fa-eye-slash");
+                $('#show_hide_password i').removeClass("fa-eye");
+            } else if ($('#show_hide_password input').attr("type") == "password") {
+                $('#show_hide_password input').attr('type', 'text');
+                $('#show_hide_password i').removeClass("fa-eye-slash");
+                $('#show_hide_password i').addClass("fa-eye");
+            }
+        });
+
+    })
+});
